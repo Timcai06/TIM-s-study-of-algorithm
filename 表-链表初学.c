74fec02cf6e9d7baf5795aad9a9206615bc804b9
Tim <1535369608@qq.com>
2025-11-20 13:10:24 +0800

    Update 表-链表初学.c

--- 表-链表初学.c
+++ 表-链表初学.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 typedef int ElemType;
-
 typedef struct node{
     ElemType data;//数据域
     struct node *next;//指针域
 }Node;
+
 //初始化链表
 Node *initList(){
     Node *head = (Node*)malloc(sizeof(Node));
@@ -54,6 +54,27 @@ int insertNode(Node *L,int pos,ElemType e){
     p->next = q;
     return 1;
 }
+//删除特定位置节点
+int deleteNode(Node *L,int pos){
+    Node *p = L;
+    int i = 0;
+    while (i < pos-1){
+        p = p->next;
+        i++;
+        if (p == NULL){
+            return 0;
+        }
+    }
+    if (p->next == NULL){
+        printf ("要删除的位置错误\n");
+        return 0;
+    }
+
+    Node *q = p->next;
+    p->next = q->next;
+    free(q);
+    return 1;
+}
 //遍历链表并输出元素
 void listNode(Node *L){
     Node *p = L->next;
@@ -65,19 +86,23 @@ void listNode(Node *L){
 }
 int main (){
     Node *list = initList();
-    //头插法检验
+    //头插法函数检验
     insertHead(list,10);
     listNode(list);
     insertHead(list,20);
     listNode(list);
     insertHead(list,30);
     listNode(list);
-    //尾插法检验
+    //尾插法函数检验
     Node *tail = get_tail(list);
     tail = insertTail(tail,10);
     listNode(list);
-    //指定位置插入检验
+    //指定位置插入函数检验
     insertNode(list,2,33);
     listNode(list);
+    //删除节点函数检验
+    deleteNode(list,3);
+    listNode(list);
+
     return 0;
 }
