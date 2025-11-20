ffd7025bea1f9759a8bd3fa7cd465a2417f1f5bc
Tim <1535369608@qq.com>
2025-11-20 13:26:42 +0800

    Update 表-链表初学.c

--- 表-链表初学.c
+++ 表-链表初学.c
@@ -56,8 +56,10 @@ int insertNode(Node *L,int pos,ElemType e){
 }
 //删除特定位置节点
 int deleteNode(Node *L,int pos){
+    //要删除的节点的前驱
     Node *p = L;
     int i = 0;
+    //遍历链表，找到要删除节点的前驱
     while (i < pos-1){
         p = p->next;
         i++;
@@ -69,11 +71,20 @@ int deleteNode(Node *L,int pos){
         printf ("要删除的位置错误\n");
         return 0;
     }
-
-    Node *q = p->next;
-    p->next = q->next;
-    free(q);
+    Node *q = p->next;//q指向要删除的节点
+    p->next = q->next;//要删除节点的前驱指向要删除节点的后继
+    free(q);//释放要删除节点的空间
     return 1;
+}
+//获取链表长度
+int listLength(Node *L){
+    Node *p = L;
+    int len = 0;
+    while (p != NULL){
+        p = p->next;
+        len++;
+    }
+    return len-1;
 }
 //遍历链表并输出元素
 void listNode(Node *L){
@@ -84,6 +95,17 @@ void listNode(Node *L){
     }
     printf ("\n");
 }
+//释放链表
+void freeList(Node *L){
+    Node *p = L->next;
+    Node *q;
+    while (p != NULL){
+        q = p->next;
+        free(p);
+        p = q;
+    }
+    L->next = NULL;
+}
 int main (){
     Node *list = initList();
     //头插法函数检验
@@ -93,16 +115,26 @@ int main (){
     listNode(list);
     insertHead(list,30);
     listNode(list);
+    printf ("目前链表的长度为%d\n",listLength(list));
+
     //尾插法函数检验
     Node *tail = get_tail(list);
     tail = insertTail(tail,10);
     listNode(list);
+    printf ("目前链表的长度为%d\n",listLength(list));
+
     //指定位置插入函数检验
     insertNode(list,2,33);
     listNode(list);
-    //删除节点函数检验
-    deleteNode(list,3);
-    listNode(list);
+    printf ("目前链表的长度为%d\n",listLength(list));
 
+    //删除节点函数检验
+    deleteNode(list,1);
+    listNode(list);
+    printf ("目前链表的长度为%d\n",listLength(list));
+
+    freeList(list);
+    listNode(list);
+    printf ("目前链表的长度为%d\n",listLength(list));
     return 0;
 }
